9dd2dd429dd61b0be16b354830e52b2b7257efb3
Alan Quadros <stefani.kopp@acad.pucrs.br>
2018-07-04 19:12:30 -0300

    a

---
+++ api/models/userModel.js
@@ -0,0 +1,26 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the user'
+  },
+  senha: {
+    type: String,
+    required: 'Kindly enter the name of the senha'
+  },
+  email: {
+    type: String,
+    required: 'Kindly enter the name of the email'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  
+});
+
+module.exports = mongoose.model('Users', UserSchema);
---
+++ api/routes/userRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var userList = require('../controllers/userController' );
+
+  // todoList Routes
+  app.route('/users')
+    .get(userList.list_all_users)
+    .post(userList.create_a_user);
+
+
+//   app.route('/tasks/:taskId')
+//     .get(todoList.read_a_task)
+//     .put(todoList.update_a_task)
+//     .delete(todoList.delete_a_task);
+};
